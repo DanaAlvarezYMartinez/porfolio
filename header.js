55459a02e5d66509d6f7f932fd7adb72dbe7bc65
DanaAlvarezYMartinez <martinezdanaalvarez@gmail.com>
2022-06-19 12:28:16 -0300

    added about-me section

--- header.js
+++ header.js
@@ -2,4 +2,10 @@ document.addEventListener('click', (e) => {
     if(e.target.classList.contains('rocket')){
         document.getElementsByTagName('header')[0].classList.toggle('hidden');
     }
+
+    if(e.target.classList.contains('about')){
+        document.getElementById('home').classList.add('hidden');
+        document.getElementById('about-me').classList.remove('hidden');
+        document.getElementsByTagName('header')[0].classList.add('hidden');
+    }
 })
