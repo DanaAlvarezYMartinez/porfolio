881b50b75512949faedea0b085934c67a580f363
DanaAlvarezYMartinez <martinezdanaalvarez@gmail.com>
2022-07-16 10:21:48 -0300

    Added header

--- header.js
+++ header.js
@@ -9,9 +9,37 @@ document.addEventListener('click', (e) => {
         document.getElementsByTagName('header')[0].classList.add('hidden');
     }
 
+    if(e.target.classList.contains('skills')){
+        document.getElementById('home').classList.add('hidden');
+        document.getElementById('skills').classList.remove('hidden');
+        document.getElementsByTagName('header')[0].classList.add('hidden');
+    }
+
+    if(e.target.classList.contains('projects')){
+        document.getElementById('home').classList.add('hidden');
+        document.getElementById('projects').classList.remove('hidden');
+        document.getElementsByTagName('header')[0].classList.add('hidden');
+    }
+
+    if(e.target.classList.contains('hobbies')){
+        document.getElementById('home').classList.add('hidden');
+        document.getElementById('hobbies').classList.remove('hidden');
+        document.getElementsByTagName('header')[0].classList.add('hidden');
+    }
+
     if(e.target.classList.contains('bb8')){
         document.getElementById('about-me').classList.add('hidden');
         document.getElementById('home').classList.remove('hidden');
         document.getElementsByTagName('header')[0].classList.add('hidden');
     }
+})
+
+var triggerTabList = [].slice.call(document.querySelectorAll('#myTab a'))
+triggerTabList.forEach(function (triggerEl) {
+  var tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', function (event) {
+    event.preventDefault()
+    tabTrigger.show()
+  })
 })
